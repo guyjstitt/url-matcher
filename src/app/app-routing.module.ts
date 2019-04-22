3a403cb91ad064675a985c593b1fd579acaa9630
Guy Stitt <guyjstitt@gmail.com>
2019-04-21 23:41:07 -0400

    adding custom url matcher for username

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
+import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, UrlSegment } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+// use a custom url matcher to regex that username starts with @
+export function username(url: UrlSegment[]) {
+  return url.length === 1 && url[0].path.match(/^@.+/g) ? ({consumed: url}) : null;
+}
+
+const routes: Routes = [
+  {
+    component: AccountComponent,
+    path: 'account/:id'
+  },
+  // naive approach to match on anything for username
+  // {
+  //   component: ProfileComponent,
+  //   path: ':username'
+  // },
+  {
+    component: ProfileComponent,
+    matcher: username
+  },
+  {
+    component: HomeComponent,
+    path: ''
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
